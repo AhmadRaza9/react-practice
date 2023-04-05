18f171a53646239d1e925cf20a40d08c4aa9ca1b
AhmadRaza9 <raza.atakiu@gmail.com>
2023-04-05 17:45:02 +0500

    adding some dummy expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 298,
+      date: new Date(2000, 1, 1).toString(),
+    },
+    {
+      title: "Car Insurance",
+      amount: 298,
+      date: new Date(2000, 1, 1).toString(),
+    },
+    {
+      title: "Car Insurance",
+      amount: 298,
+      date: new Date(2000, 1, 1).toString(),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />  
+      <ExpenseItem />
     </div>
   );
 }
