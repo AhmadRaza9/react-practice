615f03716d2e8afcb5c54d11b3867a8352e4fc66
AhmadRaza9 <raza.atakiu@gmail.com>
2023-04-05 15:03:05 +0500

    adding some basic styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
-function ExpenseItem(){
-    return (
-        <div>
-            <div>March 23th 2020</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$325</div>
-            </div>
-        </div>
-        
-    )
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 23th 2020</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$325</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
