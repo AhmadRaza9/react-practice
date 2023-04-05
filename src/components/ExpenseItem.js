02c1525639a919cc493aa7c5946ddb12d4df811b
AhmadRaza9 <raza.atakiu@gmail.com>
2023-04-05 14:38:19 +0500

    starter project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>March 23th 2020</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$325</div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default ExpenseItem;
